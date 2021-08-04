75ed066cf45b55534ee305087b4c01f7e01a916f
Kai Tietz <ktietz@anaconda.com>
2021-08-04 08:12:31 +0200

    merge 1100.0.11 from conda-forge

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,15 +3,19 @@
 mkdir build
 cd build
 
-if [[ "$target_platform" == osx* ]]; then
-    EXTRA_CMAKE_ARGS="-C $SRC_DIR/tapi/cmake/caches/apple-tapi.cmake $SRC_DIR/llvm"
-else
-    EXTRA_CMAKE_ARGS="$SRC_DIR/src/llvm"
-    # TODO: Fix this in a better way.
-    CXXFLAGS="$CXXFLAGS -I$SRC_DIR/src/llvm/projects/clang/include -I$SRC_DIR/build/projects/clang/include"
+# TODO: Fix this in a better way.
+CXXFLAGS="$CXXFLAGS -I$SRC_DIR/src/llvm/projects/clang/include -I$SRC_DIR/build/projects/clang/include"
+
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+  CMAKE_ARGS="${CMAKE_ARGS} -DCROSS_TOOLCHAIN_FLAGS_NATIVE=-DCMAKE_C_COMPILER=$CC_FOR_BUILD;-DCMAKE_CXX_COMPILER=$CXX_FOR_BUILD;-DCMAKE_EXE_LINKER_FLAGS=;-DCMAKE_MODULE_LINKER_FLAGS=;-DCMAKE_SHARED_LINKER_FLAGS=;-DCMAKE_C_FLAGS=-O2;-DCMAKE_CXX_FLAGS=-O2"
+  CMAKE_ARGS="${CMAKE_ARGS} -DLLVM_HOST_TRIPLE=$(echo $HOST | sed s/conda/unknown/g) -DLLVM_DEFAULT_TARGET_TRIPLE=$(echo $HOST | sed s/conda/unknown/g)"
 fi
 
-cmake \
+echo "LIBTOOL: ${LIBTOOL}"
+export LIBTOOL=$(which $LIBTOOL)
+echo "LIBTOOL: ${LIBTOOL}"
+
+cmake ${CMAKE_ARGS} \
     -G Ninja \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_C_COMPILER=$CC \
@@ -25,19 +29,14 @@ cmake \
     -DCMAKE_INSTALL_NAME_TOOL=$INSTALL_NAME_TOOL \
     -DTAPI_REPOSITORY_STRING=tapi-${PKG_VERSION} \
     -DLLVM_TARGETS_TO_BUILD=host \
-    -DTAPI_FULL_VERSION=10.0.0 \
+    -DTAPI_FULL_VERSION=11.0.0 \
     -DLLVM_INCLUDE_TESTS=OFF \
     -DCMAKE_BUILD_TYPE=Release \
-    $EXTRA_CMAKE_ARGS
-
-if [[ "$target_platform" == osx* ]]; then
-    ninja install-distribution
-else
-    set +e
-    ninja clangBasic -j${CPU_COUNT}
-    ninja clangBasic -j${CPU_COUNT}
-    ninja libtapi -j${CPU_COUNT}
-    ninja libtapi -j${CPU_COUNT}
-    set -e
-    ninja install-libtapi install-tapi-headers -j${CPU_COUNT}
-fi
+    $SRC_DIR/src/llvm
+set +e
+ninja clangBasic -j${CPU_COUNT}
+ninja clangBasic -j${CPU_COUNT}
+ninja libtapi -j${CPU_COUNT}
+ninja libtapi -j${CPU_COUNT}
+set -e
+ninja install-libtapi install-tapi-headers -j${CPU_COUNT}
